d6aa641218ee87830026faaef8b0021846928efd
Sklyar13OFF <dansklyarov13@gmail.com>
2026-01-29 19:33:40 +0100

    Update urls.py

--- checklist/urls.py
+++ checklist/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/admin/', admin.site.urls),
     path('api/auth/', include('users.urls')),  
     path('api/organisation/', include('organisation.urls')), 
 
